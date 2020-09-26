ab8efb5db2d96429500bb8949fd9d23dc2d25040
Tori Fife <torifife@gmail.com>
2020-09-26 16:33:33 -0600

    complete week 2 assignment

---
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent implements OnInit {
+  contact: Contact;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact("1", 
+      "R. Kent Jackson",
+      "jacksonk@byui.edu",
+      "208-496-3771",
+      "https://web.byui.edu/Directory/Employee/jacksonk.jpg",
+      null
+    ),
+    new Contact(
+      "2",    
+      "Rex Barzee",
+      "barzeer@byui.edu",
+      "208-496-3768",
+      "https://web.byui.edu/Directory/Employee/barzeer.jpg",
+      null
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
